6333bb98917dd3a73ace00e43e5094eeb146bd74
Artem <a.baburkin@it.adcombo.com>
2023-09-03 18:41:49 +0300

    first commit

---
+++ pages/registration_page.py
@@ -0,0 +1,78 @@
+from selene import browser, be, have, command
+import os
+
+
+class RegistrationPage():
+    def __init__(self):
+        self.open()
+
+    def open(self):
+        browser.open('https://demoqa.com/automation-practice-form')
+        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
+            have.size_greater_than_or_equal(3)
+        )
+        browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+        return self
+
+    def fill_first_name(self, value):
+        browser.element('[id="firstName"]').should(be.blank).type(value)
+
+    def fill_last_name(self, value):
+        browser.element('[id="lastName"]').should(be.blank).type(value)
+
+    def fill_email(self, value):
+        browser.element('[id="userEmail"]').should(be.blank).type(value)
+
+    def choose_gender(self):
+        browser.element('[for="gender-radio-1"]').click()
+
+    def fill_user_phonenumber(self, value):
+        browser.element('[id="userNumber"]').should(be.blank).type(value)
+
+    def choose_birth_date(self, year, month, day):
+        browser.element('[id="dateOfBirthInput"]').click()
+        browser.element(f'[class="react-datepicker__month-select"]>option[value="{month}"]').click()
+        browser.element(f'[class="react-datepicker__year-select"]>option[value="{year}"]').click()
+        browser.element(f'.react-datepicker__day--00{day}').click()
+
+    def type_subject(self, value):
+        browser.element('[id="subjectsInput"]').click().type(value).press_enter()
+
+    def choose_hobbie(self, value):
+        browser.element(f'[for="hobbies-checkbox-{value}"]').click()
+
+    def choose_picture(self, path):
+        browser.element('[id="uploadPicture"]').send_keys(os.path.abspath(path))
+
+    def type_current_adress(self, value):
+        browser.element('[id="currentAddress"]').type(value)
+
+    def is_enabled(self, element):
+        if browser.element(element).should(be.enabled):
+            return True
+        else:
+            raise ValueError
+
+    def choose_state(self, value):
+        browser.element('[id="react-select-3-input"]').type(value).press_enter()
+
+    def choose_city(self, value):
+        browser.element('[id="react-select-4-input"]').type(value).press_enter()
+
+    def press_submit(self):
+        browser.element('[id="submit"]').click()
+
+    def should_have_registered(self, full_name, email, gender, mobile, date_of_birth, subjects, hobbies, picture,
+                               address, state_and_city):
+        browser.element('[class="table-responsive"]>table>tbody').should(have.exact_text(
+            f"""Student Name {full_name}
+Student Email {email}
+Gender {gender}
+Mobile {mobile}
+Date of Birth {date_of_birth}
+Subjects {subjects}
+Hobbies {hobbies}
+Picture {picture}
+Address {address}
+State and City {state_and_city}"""
+        ))
---
+++ tests/test_registration_user.py
@@ -0,0 +1,36 @@
+from pages.registration_page import RegistrationPage
+
+first_name = 'Ivan'
+last_name = 'Ivanov'
+email = 'qwerty@mail.ru'
+phone = "8800555353"
+current_address = 'CUrrent aDDress 12a Here'
+state = "NCR"
+city = "Delhi"
+
+
+def test_registration_students_form_positive():
+    registration_page = RegistrationPage()
+    registration_page.fill_first_name(first_name)
+    registration_page.fill_last_name(last_name)
+    registration_page.fill_email(email)
+    registration_page.choose_gender()
+    registration_page.fill_user_phonenumber(phone)
+    registration_page.choose_birth_date('2000', '5', '1')
+    registration_page.type_subject('Math')
+    registration_page.choose_hobbie('2')
+    registration_page.choose_hobbie('3')
+    registration_page.choose_picture(f'../data/test.png')
+    registration_page.type_current_adress(current_address)
+    registration_page.choose_state(state)
+    registration_page.is_enabled('[id="react-select-4-input"]')
+    registration_page.choose_city(city)
+    registration_page.press_submit()
+
+    #     Проверка итоговой таблицы
+    registration_page.should_have_registered(full_name=f"{first_name} {last_name}",
+                                             email=email, gender='Male', mobile=phone,
+                                             date_of_birth='01 June,2000',
+                                             subjects='Maths', hobbies="Reading, Music",
+                                             picture="test.png", address=current_address,
+                                             state_and_city=f"{state} {city}")
