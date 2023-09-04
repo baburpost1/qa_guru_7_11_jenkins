e042cf10f734181fbf05b2343b1748d936921c1d
Artem <a.baburkin@it.adcombo.com>
2023-09-04 20:55:49 +0300

    fix allure and requirements

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,13 +1,11 @@
 from selene import browser, be, have, command
 import os
 from tests.conftest import RESOURCE_PATH
-from tests.conftest import setup_browser
 
 class RegistrationPage():
     def __init__(self):
         self.open()
 
-    browser = setup_browser
     def open(self):
         browser.open('https://demoqa.com/automation-practice-form')
         browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
