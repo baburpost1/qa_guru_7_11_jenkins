2a4eacdd8155d8ffe9de2e5aa2ad5f6c78b17560
Artem <a.baburkin@it.adcombo.com>
2023-09-05 21:39:30 +0300

    trach commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,32 +10,32 @@ from utils import attach
 FILE_PATH = os.path.abspath(__file__)
 PROJECT_PATH = os.path.dirname(FILE_PATH)
 RESOURCE_PATH = os.path.join(PROJECT_PATH, 'resources')
-
-
-@pytest.fixture(autouse=True)
-def setup_browser():
-    browser_version = "100.0"
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-    browser = Browser(Config(driver))
-    yield browser
-
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
-
-    browser.quit()
+#
+#
+# @pytest.fixture(autouse=True)
+# def setup_browser():
+#     browser_version = "100.0"
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": browser_version,
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+#
+#     browser = Browser(Config(driver))
+#     yield browser
+#
+#     attach.add_screenshot(browser)
+#     attach.add_logs(browser)
+#     attach.add_html(browser)
+#     attach.add_video(browser)
+#
+#     browser.quit()
