c83b3cec02bce659217db93688aafe60e5c70216
Artem <a.baburkin@it.adcombo.com>
2023-09-08 08:43:34 +0300

    final commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,42 +10,12 @@ from selene.support.shared import browser
 FILE_PATH = os.path.abspath(__file__)
 PROJECT_PATH = os.path.dirname(FILE_PATH)
 RESOURCE_PATH = os.path.join(PROJECT_PATH, 'resources')
-#
-#
-# @pytest.fixture(autouse=True)
-# def setup_browser():
-#     browser_version = "100.0"
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": browser_version,
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#
-#     browser = Browser(Config(driver))
-#     yield browser
-#
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     attach.add_video(browser)
-#
-#     browser.quit()
+
 @pytest.fixture(autouse=True)
 def setup_browser():
-    browser.config.base_url = 'https://demoqa.com'
-    # driver_options = browser.config.driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
-    # driver_options.add_argument('--disable-notifications')
-    # browser.config.driver_options = driver_options
+
+    # Список всех доступных парамеров https://peter.sh/experiments/chromium-command-line-switches/
+
     browser.config.window_height = 1400
     browser.config.window_width = 1600
     browser_version = "100.0"
@@ -70,8 +40,7 @@ def setup_browser():
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
     attach.add_video(browser)
+    attach.add_logs(browser)
 
     browser.quit()
--- tests/test_registration_user.py
+++ tests/test_registration_user.py
@@ -1,5 +1,5 @@
 from pages.registration_page import RegistrationPage
-from selene.support.shared import browser
+import allure
 
 first_name = 'Ivan'
 last_name = 'Ivanov'
@@ -11,27 +11,40 @@ city = "Delhi"
 
 
 def test_registration_students_form_positive(setup_browser):
-    registration_page = RegistrationPage()
-    registration_page.fill_first_name(first_name)
-    registration_page.fill_last_name(last_name)
-    registration_page.fill_email(email)
-    registration_page.choose_gender()
-    registration_page.fill_user_phonenumber(phone)
-    registration_page.choose_birth_date('2000', '5', '1')
-    registration_page.type_subject('Math')
-    registration_page.choose_hobbie('2')
-    registration_page.choose_hobbie('3')
-    registration_page.choose_picture('test.png')
-    registration_page.type_current_adress(current_address)
-    registration_page.choose_state(state)
-    registration_page.is_enabled('[id="react-select-4-input"]')
-    registration_page.choose_city(city)
-    registration_page.press_submit()
+    with allure.step("Открытие страницы регистрации"):
+        registration_page = RegistrationPage()
+    with allure.step("Заполнение имени"):
+        registration_page.fill_first_name(first_name)
+    with allure.step("Заполнение фамилии"):
+        registration_page.fill_last_name(last_name)
+    with allure.step("Заполнение email"):
+        registration_page.fill_email(email)
+    with allure.step("Выбор пола"):
+        registration_page.choose_gender()
+    with allure.step("Заполнение мобильного телефона"):
+        registration_page.fill_user_phonenumber(phone)
+    with allure.step("Выбор даты рождения"):
+        registration_page.choose_birth_date('2000', '5', '1')
+    with allure.step("Выбор предметов"):
+        registration_page.type_subject('Math')
+    with allure.step("Выбор хобби"):
+        registration_page.choose_hobbie('2')
+        registration_page.choose_hobbie('3')
+    with allure.step("Загрузка изображения"):
+        registration_page.choose_picture('test.png')
+    with allure.step("Заполнения адреса проживания"):
+        registration_page.type_current_adress(current_address)
+    with allure.step("Выбор Штата и города"):
+        registration_page.choose_state(state)
+        registration_page.is_enabled('[id="react-select-4-input"]')
+        registration_page.choose_city(city)
+    with allure.step("Нажатие кнопки подтвердить"):
+        registration_page.press_submit()
 
-    #     Проверка итоговой таблицы
-    registration_page.should_have_registered(full_name=f"{first_name} {last_name}",
-                                             email=email, gender='Male', mobile=phone,
-                                             date_of_birth='01 June,2000',
-                                             subjects='Maths', hobbies="Reading, Music",
-                                             picture="test.png", address=current_address,
-                                             state_and_city=f"{state} {city}")
+    with allure.step("Проверка итоговой таблицы"):
+        registration_page.should_have_registered(full_name=f"{first_name} {last_name}",
+                                                 email=email, gender='Male', mobile=phone,
+                                                 date_of_birth='01 June,2000',
+                                                 subjects='Maths', hobbies="Reading, Music",
+                                                 picture="test.png", address=current_address,
+                                                 state_and_city=f"{state} {city}")
